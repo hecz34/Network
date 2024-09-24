f84436824fd5e3e92b7466e8fbe9b3ed4b3f8f29
Hector Barrientos <m.hec12@gmail.com>
2024-09-24 10:10:18 -0500

    Add following field to User model and create Post model to store details about posts

--- network/models.py
+++ network/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField(
+        "self", related_name="followers", symmetrical=False, blank=True
+    )
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
